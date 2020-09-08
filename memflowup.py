7c6f2e34b263b7e509611a0dd514bd6565bcd7cd
Aurimas Blažulionis <0x60@pm.me>
2020-09-08 20:39:16 +0300

    Update the script to store installed connectors, open up /dev/tty on unix, and more

--- memflowup.py
+++ memflowup.py
@@ -3,6 +3,10 @@
 import sys, shutil, io, tarfile, json, os, subprocess, urllib.request as request
 from os.path import expanduser, join, basename
 
+# Setup stdout in case the script was piped in on *nix
+if os.path.isfile('/dev/tty'):
+    sys.stdin = os.fdopen('/dev/tty', 'r')
+
 connectors = ['memflow-qemu-procfs', 'memflow-coredump', 'memflow-daemon-connector', 'memflow-kvm']
 
 registry = 'https://crates.io/api/v1/crates'
@@ -51,11 +55,18 @@ def get_installed_connectors():
     try:
         with open(get_connector_db_path()) as f:
             data = json.load(f)
-            print(data)
+            return data
     except:
-        return []
+        return {}
 
-installed_connectors = get_installed_connectors();
+installed_connectors = get_installed_connectors()
+
+def save_connectors():
+    try:
+        with open(get_connector_db_path(), 'w') as f:
+            json.dump(installed_connectors, f)
+    except:
+        print('error saving connector db')
 
 def cargo_build(connector):
     os.chdir(join(connector_cache, '%s-%s'%connector))
@@ -81,43 +92,82 @@ def install_connector(connector):
         print('crate file can not be name the same as the database file!')
     else:
         r = request.urlopen("%s/%s/%s/download"%(registry, connector[0], connector[1])).read()
-        untar(connector_cache, r);
+        untar(connector_cache, r)
         built_file = cargo_build(connector)
         base_name = basename(built_file)
         shutil.copy2(built_file, join(output, base_name))
         print(built_file)
+        installed_connectors[connector[0]] = connector[1]
+        save_connectors()
+
+def select_crate(crates, val):
+    if val.isdigit():
+        i = int(val)
+        if len(crates) > i:
+            return crates[i]
+        else:
+            return None
+    else:
+        for c in crates:
+            if c[0] == val:
+                return c
+        return None
 
 def install_new_connectors():
     crates = []
+    key_crates = {}
 
     print('Available crates:')
     
     for i, c in enumerate(connectors):
         info = get_crate_info(c)
-        print('%d) %s %s' % (i, info[0], info[1]))
+        if info[0] in installed_connectors:
+            installed_str = ' [installed %s]'%installed_connectors[info[0]]
+        else:
+            installed_str = ''
+        print('%d) %s %s%s' % (i, info[0], info[1], installed_str))
         crates.append(info)
-
+    
     print('')
-    inp = input('Select which crates to install (space separated list, or * for all): ')
-    
-    if inp.rstrip() == '*':
-        selected = [i for i in range(len(crates))]
-    else:
-        selected = [int(i) for i in inp.split(' ')]
-    
-    print(selected)
 
-    for i in selected:
-        install_connector(crates[i])
+    if len(sys.argv) > 2 and sys.argv[2] == '-n':
+        inp = sys.argv[3:]
+    else:
+        inp = input('Select which crates to install (space separated list of numbers/names, or * for all): ')
+        inp = inp.rstrip().split(' ')
+
+    if len(inp) == 0:
+        print('none selected!')
+        return
+    
+    if inp[0] == '*':
+        selected = [crates[i] for i in range(len(crates))]
+    else:
+        selected = [select_crate(crates, i) for i in inp]
+
+    for c in selected:
+        if c == None:
+            print('invalid value entered!')
+            return
+    
+    for c in selected:
+        install_connector(c)
 
     print("done")
 
 def update_connectors():
 
-    for c in installed_connectors:
-        nc = get_crate_info(c[0])
-        if nc[1] != c[1]:
+    if '-f' in sys.argv:
+        force = True
+    else:
+        force = False
+
+    for k in installed_connectors:
+        nc = get_crate_info(k)
+        if force or nc[1] != installed_connectors[k]:
             install_connector(nc)
+        else:
+            print('%s is already up to date (%s)'%(nc[0], nc[1]))
 
     print("done")
 
@@ -125,11 +175,18 @@ memflow_crate = get_crate_info('memflow')
 
 try:
     print('Latest memflow version: %s\n'%memflow_crate[1])
+
+    print('Installed connectors:')
+
+    for k in installed_connectors:
+        print('%s %s'%(installed_connectors[k], k))
+
+    print('')
     
     if len(sys.argv) > 1:
         op = sys.argv[1]
     else:
-        op = ""
+        op = input('select operation (install, update): ')
     
     print(op)
     
