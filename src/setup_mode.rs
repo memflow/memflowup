6c75241c35b3451e60efa181184048515980fdb1
ko1N <ko1N1337@gmail.com>
2020-12-01 22:00:14 +0100

    Documented github apis

--- src/setup_mode.rs
+++ src/setup_mode.rs
@@ -1,10 +1,10 @@
 use crate::util;
 
 use std::env;
-use std::fs;
-use std::process::{Command, Stdio};
-use std::path::Path;
 use std::ffi::OsStr;
+use std::fs;
+use std::path::Path;
+use std::process::{Command, Stdio};
 
 use log::{info, warn};
 
@@ -13,11 +13,15 @@ pub fn setup_mode() {
     ensure_rust();
 
     // 2. ask the user what packages he wants to install (filtered by the current OS)
+    install_connector_branch("memflow-coredump", "master");
+
     // 2.1. ask user if he wants nightly or stable versions
+
     // 3. install connectors (with connector specific quirks / additional dependencies)
     // 3.1 download the corresponding git repository in a temp folder, at best at the latest tagged version
     // 3.2 compile the connector and install it
     // 3.3 ask the user wether to put them into global / local directory -> ask for sudo permissions on linux
+
     // 4. done :)
 }
 
@@ -86,4 +90,57 @@ fn install_rustup() {
         .stderr(Stdio::inherit())
         .output()
         .expect("failed to execute rustup script");
+}
+
+/// Installs the given connector from git
+fn install_connector_branch(connector_name: &str, connector_branch: &str) {
+    // 1. we need to query all available branches
+    // https://api.github.com/repos/memflow/memflow-coredump/branches
+
+    // if we want to install a stable version of the connector
+    // we should pick the latest tag or master branch via this api:
+    // https://api.github.com/repos/memflow/memflow-coredump/tags
+    // this will also give us a zip file in the zipball_url / tarball_url
+
+    // if we want to install a specific branch just download the branch
+    // specific branches can then be downloaded as follows:
+    // https://github.com/memflow/memflow-coredump/archive/master.tar.gz
+
+    info!("downloading connector: {}", connector_name);
+    let mut connector_path = env::temp_dir();
+    connector_path.push(format!("{}_{}.tar.gz", connector_name, connector_branch));
+    let connector_zip = util::download_file(&format!(
+        "https://api.github.com/repos/memflow/{}/tarball/{}",
+        connector_name, connector_branch
+    ))
+    .expect("unable to download connector archive");
+    fs::write(connector_path.clone(), connector_zip)
+        .expect("unable to write rustup script to temp directory");
+
+    // 2. unzip the archive in the temp directory
+    // TODO: use crates.io zip = "0.5"
+    Command::new("tar")
+        .arg("-xvf")
+        .arg(connector_path.clone())
+        .arg("--strip")
+        .arg("1")
+        .arg("--directory")
+        .arg(format!("{}-{}", connector_name, connector_branch))
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output()
+        .expect("failed to extract connector tarball");
+
+    // 3. run `cargo build --release --all-features` in the folder
+    let mut connector_out_path = env::temp_dir();
+    connector_out_path.push(format!("{}-{}", connector_name, connector_branch));
+    Command::new("cargo")
+        .arg("build")
+        .arg("--release")
+        .arg("--all-features")
+        .current_dir(connector_out_path)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output()
+        .expect("failed to compile connector");
 }
