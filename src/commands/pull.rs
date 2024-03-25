cb7f6af9f1150f2c1c74a57ab94ef940347d34b1
ko1N <ko1N1337@gmail.com>
2024-03-25 02:45:19 +0100

    Added ability to pull multiple and also all available plugins at once

--- src/commands/pull.rs
+++ src/commands/pull.rs
@@ -17,11 +17,16 @@ fn to_http_err<S: ToString>(err: S) -> Error {
 #[inline]
 pub fn metadata() -> Command {
     Command::new("pull").args([
-        Arg::new("plugin_uri").required(true).action(ArgAction::Set),
+        Arg::new("plugin_uri").action(ArgAction::Append),
+        Arg::new("all")
+            .short('a')
+            .long("all")
+            .help("pulls the latest version of all available plugins")
+            .action(ArgAction::SetTrue),
         Arg::new("force")
             .short('f')
             .long("force")
-            .help("forces download of the plugin even if it is already installed.")
+            .help("forces download of the plugin even if it is already installed")
             .action(ArgAction::SetTrue),
         Arg::new("pub-key")
             .short('p')
@@ -32,10 +37,46 @@ pub fn metadata() -> Command {
 }
 
 pub async fn handle(matches: &ArgMatches) -> Result<()> {
-    let plugin_uri = matches.get_one::<String>("plugin_uri").unwrap();
+    let plugin_uris = matches
+        .get_many::<String>("plugin_uri")
+        .unwrap_or_default()
+        .cloned()
+        .collect::<Vec<_>>();
+    let all = matches.get_flag("all");
     let force = matches.get_flag("force");
     let pub_key = matches.get_one::<String>("pub-key");
 
+    // TODO: support custom registry for wildcard
+    if all {
+        let plugins = memflow_registry_client::plugins(None).await?;
+        for plugin in plugins.iter() {
+            if let Err(err) = pull(&plugin.name, force, pub_key.map(String::as_str)).await {
+                println!(
+                    "{} Error downloading plugin {:?}: {}",
+                    console::style("[X]").bold().dim().red(),
+                    plugin.name,
+                    err
+                );
+            }
+        }
+    } else {
+        // TODO: parallel downloads
+        for plugin_uri in plugin_uris.iter() {
+            if let Err(err) = pull(&plugin_uri, force, pub_key.map(String::as_str)).await {
+                println!(
+                    "{} Error downloading plugin {:?}: {}",
+                    console::style("[X]").bold().dim().red(),
+                    plugin_uri,
+                    err
+                );
+            }
+        }
+    }
+
+    Ok(())
+}
+
+async fn pull(plugin_uri: &str, force: bool, pub_key: Option<&str>) -> Result<()> {
     // load the signature verifier
     let verifier = if let Some(pub_key) = pub_key {
         // load custom public key
@@ -48,9 +89,6 @@ pub async fn handle(matches: &ArgMatches) -> Result<()> {
     // find the correct plugin variant based on the input arguments
     let plugin_uri: PluginUri = plugin_uri.parse()?;
     let variant = memflow_registry_client::find_by_uri(&plugin_uri).await?;
-
-    // query file
-    let response = memflow_registry_client::download(&plugin_uri, &variant).await?;
 
     // check if file already exists
     let file_name = plugin_file_name(&variant);
@@ -65,7 +103,7 @@ pub async fn handle(matches: &ArgMatches) -> Result<()> {
                 console::style("[X]").bold().dim().red(),
                 file_name.file_name().unwrap()
             );
-            return Err(Error::AlreadyExists("plugin already installed".to_owned()));
+            return Ok(());
         } else {
             println!(
                 "{} Plugin {:?} already exists with a different checksum, redownloading.",
@@ -80,6 +118,9 @@ pub async fn handle(matches: &ArgMatches) -> Result<()> {
         console::style("[-]").bold().dim(),
         file_name.file_name().unwrap()
     );
+
+    // query file
+    let response = memflow_registry_client::download(&plugin_uri, &variant).await?;
 
     // download the file to memory
     let mut buffer = BytesMut::new();
