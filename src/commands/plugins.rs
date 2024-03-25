46aee98f22ab28cdf3ee0ed9c493edfc81d0be1d
ko1N <ko1N1337@gmail.com>
2024-03-25 16:04:57 +0100

    Added purge command which removes all orphaned plugins with no .meta file attached

--- src/commands/plugins.rs
+++ src/commands/plugins.rs
@@ -5,7 +5,7 @@ use clap::{Arg, ArgAction, ArgMatches, Command};
 use memflow_registry_client::shared::PluginVariant;
 
 use crate::{
-    commands::plugins_path,
+    commands::{plugin_extension, plugins_path},
     error::{Error, Result},
 };
 
@@ -29,7 +29,7 @@ pub async fn handle(matches: &ArgMatches) -> Result<()> {
                 if let Some(extension) = path.path().extension() {
                     if extension.to_str().unwrap_or_default() == "meta" {
                         if let Ok(metadata) = serde_json::from_str::<PluginVariant>(
-                            &std::fs::read_to_string(path.path())?,
+                            &tokio::fs::read_to_string(path.path()).await?,
                         ) {
                             let file_metadata = tokio::fs::metadata(path.path()).await?;
                             let datetime: DateTime<Utc> = file_metadata.created()?.into();
@@ -42,19 +42,93 @@ pub async fn handle(matches: &ArgMatches) -> Result<()> {
                                 datetime.naive_utc().to_string(),
                             );
                         } else {
-                            // warn
+                            // TODO: print warning about orphaned plugin and give hints
+                            // on how to install plugins from source with memflowup
                         }
                     }
                 }
             }
         }
         Some(("purge", _)) => {
-            println!("removing all plugins but the latest installed version");
             // TODO: find and clean all files that have no .meta file
             // TODO: allow purging of everything
+            let orphaned = remove_orphaned_plugins().await?;
+            println!(
+                "{} Plugins purged, removed {} plugins.",
+                console::style("[=]").bold().dim().green(),
+                orphaned,
+            );
         }
         _ => unreachable!(),
     }
 
     Ok(())
+}
+
+/// Removes all plugins which do not have a proper .meta file associated with them.
+async fn remove_orphaned_plugins() -> Result<usize> {
+    let mut orphaned_plugins = 0;
+
+    let paths = std::fs::read_dir(plugins_path())?;
+    for path in paths.filter_map(|p| p.ok()) {
+        if let Some(extension) = path.path().extension() {
+            // TODO: should we only check for plugin_extension here?
+            if extension.to_str().unwrap_or_default() == plugin_extension() {
+                // check if the corresponding .meta file exists
+                let mut meta_file_name = path.path();
+                meta_file_name.set_extension("meta");
+
+                let orphaned = if meta_file_name.exists() {
+                    if let Ok(metadata) = serde_json::from_str::<PluginVariant>(
+                        &tokio::fs::read_to_string(meta_file_name).await?,
+                    ) {
+                        let bytes = tokio::fs::read(path.path()).await?;
+                        let digest = sha256::digest(&bytes[..]);
+                        if metadata.digest == digest {
+                            None
+                        } else {
+                            // digest in .meta is not matching file on disk
+                            Some("checksum mismatch in .meta file")
+                        }
+                    } else {
+                        // invalid .meta file
+                        Some("corrupted .meta file")
+                    }
+                } else {
+                    // .meta file does not exist
+                    Some(".meta file missing")
+                };
+
+                if let Some(reason) = orphaned {
+                    // TODO: try parse metafile and check digest to be triple sure
+                    println!(
+                        "{} Deleting orphaned plugin: {:?} ({})",
+                        console::style("[=]").bold().dim().green(),
+                        path.path().as_os_str(),
+                        reason
+                    );
+
+                    // remove plugin
+                    tokio::fs::remove_file(path.path()).await?;
+
+                    // try to remove meta file (this is allowed to fail)
+                    let mut meta_file_name = path.path();
+                    meta_file_name.set_extension("meta");
+                    if let Err(err) = tokio::fs::remove_file(meta_file_name).await {
+                        println!(
+                            "{} Unable to delete .meta file for plugin {:?}: {}",
+                            console::style("[X]").bold().dim().red(),
+                            path.path().file_name().unwrap_or_default().to_os_string(),
+                            err
+                        );
+                    }
+
+                    orphaned_plugins += 1;
+                    continue;
+                }
+            }
+        }
+    }
+
+    Ok(orphaned_plugins)
 }
